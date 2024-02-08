33336f7c3090cbe678058779dc3e5e0e8cd6dda5
Bibek Panthi <bpanthi977@gmail.com>
2024-02-08 20:35:31 +0545

    🎁 chainValidators

--- src/FormUtils.tsx
+++ src/FormUtils.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { FieldController, StateController, Structure } from "./index.js";
+import { FieldController, IntegerValidator, SetValidator, StateController, StringValidator, Structure, Validator, ValidatorInput, ValidatorOutput } from "./index.js";
 import { ReferenceMultiple } from "@bhoos/game-kit-ui";
 
 export function isErrorFree(ctr: StateController<any> | FieldController<any>) {
@@ -22,4 +22,25 @@ export function useStateController<T extends Structure>(init: () => StateControl
   const [controller, _] = useState(init);
   ReferenceMultiple.use(controller.ref);
   return controller;
+}
+
+
+// Learnt about this technique of chaining types from
+// https://stackoverflow.com/questions/70873524/chaining-function-types-in-typescript
+type First<T extends any[]> = T extends [infer F, ...infer _] ? F : never;
+type Last<T extends any[]> =  T extends [...infer _, infer L] ? L : never;
+type RightShift<T extends any[]> = Extract<[Validator<any, any>, ...{ [I in keyof T]: T[I] }], Record<keyof T, any>>;
+type Next<T extends any[], Index extends keyof T> = RightShift<T>[Index];
+
+type CheckChain<T extends any[]> = T extends { [I in keyof T]: Validator<ValidatorOutput<Next<T, I>>, any> } ? true: false;
+type ChainRet<T extends any[]> = Validator<ValidatorInput<First<T>>, ValidatorOutput<Last<T>>>
+
+type ChainCheckNRet<T extends any[]> = CheckChain<T> extends true ? ChainRet<T>: never;
+
+export function chainValidators<T extends Validator<any, any>[]>(...validators: T): ChainCheckNRet<T> {
+  return {
+    validate: (input: ValidatorInput<First<T>>) =>  {
+      return validators.reduce((value, validator) => validator.validate(value), input)
+    }
+  } as ChainCheckNRet<T>
 }
